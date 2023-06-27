fb596a094a77279143742a718f290c410ddfd9c9
Jan Krajnak <janci.krajnak@gmail.com>
2023-06-27 06:33:02 +0200

    ViewportTrap package.json info and readme.md

--- packages/viewport-trap/vite.config.ts
+++ packages/viewport-trap/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import dts from 'vite-plugin-dts'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { joinPathFragments } from '@nx/devkit'
 
 export default defineConfig({
@@ -18,16 +19,15 @@ export default defineConfig({
     viteTsConfigPaths({
       root: '../../',
     }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'README.md',
+          dest: './',
+        }
+      ]
+    })
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
